112c37e980d7c4c93811a1e48ccd1fdc2788b1f3
DenisProgrammer <30808396+scisoftdev@users.noreply.github.com>
2020-01-17 16:45:05 +0400

    Create web.php

---
+++ config/web.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'id' => 'video',
+    'basePath' => realpath(__DIR__ . '/../'),
+    'components' => [
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            'rules' => [
+
+            ],
+        ]
+    ]
+];
