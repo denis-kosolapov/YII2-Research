cc4a741cfa40af69b8ceaf11ed5b8a828fac83b3
DenisProgrammer <30808396+scisoftdev@users.noreply.github.com>
2020-01-17 16:45:44 +0400

    Create SiteController.php

---
+++ controller/SiteController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace app\controllers;
+
+use \yii\web\Controller;
+
+class SiteController extends Controller {
+    public function actionIndex()
+    {
+        return $this -> render('index');
+    }
+
+    public function actionJoin()
+    {
+        return $this -> render('join');
+    }
+
+    public function actionLogin()
+    {
+        return $this -> render('login');
+    }
+}
